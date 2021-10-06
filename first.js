009fd7af98ae7430b44d8c8436ea05b931914818
C G Mitun Akil <78992845+cg-mitun-akil@users.noreply.github.com>
2021-10-06 16:25:27 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,48 @@
+let command = document.getElementById("error")
+function purchase()
+{
+    alert("Unknown error")
+    command.innerText += "Error "
+} 
+
+
+
+// let myage = 19
+// console.log(myage)
+// let humdogRatio = 7
+// let dogAge = myage*humdogRatio
+// console.log(dogAge)
+// let bonus = 50
+// bonus = bonus + 50
+// console.log(bonus)
+// bonus = bonus - 75
+// console.log(bonus)
+// bonus = bonus + 45
+// console.log(bonus)
+// function print()
+// {
+//     console.log(42)
+// }
+// print()
+// let count = 0
+// let prevSave = document.getElementById("History")
+// let countEl = document.getElementById("count-el")
+// function increment()
+// {
+//     count = count + 1
+//     countEl.textContent = count    
+// }
+// function save()
+// {
+//     console.log(count);
+//     prevSave.textContent +=  count + " - ";
+//     count = 0
+//     countEl.textContent = count 
+    
+// }
+// let welcomeEl = document.getElementById("welcome-el")
+// let greet = "Welcome Back, "
+// let username = "C G Mitun Akil"
+// welcomeEl.innerText = greet + username
+// console.log(welcomeEl)
+
